bbc3ca8f323a9f77e933fb16adcb5670bb8092e9
sjoerdsommen <sjoerdsommen@gmail.com>
2021-09-09 17:10:53 +0200

    connected with ganache gui and inserted MyToken contract

---
+++ react/contracts/MyToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Cappuciono token", "CAPPU") {
+        _mint(msg.sender, initialSupply);
+    }
+}
--- react/scripts/deploy.py
+++ react/scripts/deploy.py
@@ -1,4 +1,4 @@
-from brownie import SolidityStorage, VyperStorage, accounts, network
+from brownie import MyToken, accounts, network
 
 
 def main():
@@ -6,11 +6,4 @@ def main():
     if network.show_active()=='development':
         # add these accounts to metamask by importing private key
         owner = accounts[0]
-        SolidityStorage.deploy({'from':accounts[0]})
-        VyperStorage.deploy({'from':accounts[0]})
-
-    elif network.show_active() == 'kovan':
-        # add these accounts to metamask by importing private key
-        owner = accounts.load("main")
-        SolidityStorage.deploy({'from':owner})
-        VyperStorage.deploy({'from':owner})
+        MyToken.deploy(1000, {'from':accounts[0]})
