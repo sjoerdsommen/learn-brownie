89b84067ad4c287fd329c4eb45e7394ed8132f8a
sjoerdsommen <sjoerdsommen@gmail.com>
2021-09-10 13:58:30 +0200

    started with the react app

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -6,27 +6,17 @@ import {getEthereum} from "./getEthereum"
 
 class App extends Component {
 
-    state = {
-        web3: null,
-        accounts: null,
-        chainid: null,
-        vyperStorage: null,
-        vyperValue: 0,
-        vyperInput: 0,
-        solidityStorage: null,
-        solidityValue: 0,
-        solidityInput: 0,
-    }
+    state = { loaded:false }
 
     componentDidMount = async () => {
 
         // Get network provider and web3 instance.
-        const web3 = await getWeb3()
+        this.web3 = await getWeb3()
 
         // Try and enable accounts (connect metamask)
         try {
-            const ethereum = await getEthereum()
-            ethereum.enable()
+            this.ethereum = await getEthereum()
+            this.ethereum.enable()
         } catch (e) {
             console.log(`Could not enable accounts. Interaction with contracts not available.
             Use a modern browser with a Web3 plugin to fix this issue.`)
@@ -34,16 +24,12 @@ class App extends Component {
         }
 
         // Use web3 to get the user's accounts
-        const accounts = await web3.eth.getAccounts()
+        this.accounts = await this.web3.eth.getAccounts()
 
         // Get the current chain id
-        const chainid = parseInt(await web3.eth.getChainId())
+        this.chainid = parseInt(await this.web3.eth.getChainId())
 
-        this.setState({
-            web3,
-            accounts,
-            chainid
-        }, await this.loadInitialContracts)
+        this.setState({}, await this.loadInitialContracts)
 
     }
 
@@ -63,21 +49,20 @@ class App extends Component {
             _chainID = "dev"
         }
         console.log(_chainID)
-        const vyperStorage = await this.loadContract(_chainID,"VyperStorage")
-        const solidityStorage = await this.loadContract(_chainID,"SolidityStorage")
+        this.MyToken = await this.loadContract(_chainID,"MyToken")
+        this.MyTokenSale = await this.loadContract(_chainID,"MyTokenSale")
+        this.KycContract = await this.loadContract(_chainID,"KycContract")
 
-        if (!vyperStorage || !solidityStorage) {
+        if (!this.MyToken || !this.MyTokenSale || !this.KycContract) {
             return
         }
 
-        const vyperValue = await vyperStorage.methods.get().call()
-        const solidityValue = await solidityStorage.methods.get().call()
+        this.MyTokenValue = await this.MyToken.methods.get().call()
+        this.MyTokenSaleValue = await this.MyTokenSale.methods.get().call()
+        this.KycContractValue = await this.KycContract.methods.get().call()
 
         this.setState({
-            vyperStorage,
-            vyperValue,
-            solidityStorage,
-            solidityValue,
+            loaded:true
         })
     }
 
@@ -106,59 +91,53 @@ class App extends Component {
         return new web3.eth.Contract(contractArtifact.abi, address)
     }
 
-    changeVyper = async (e) => {
-        const {accounts, vyperStorage, vyperInput} = this.state
-        e.preventDefault()
-        const value = parseInt(vyperInput)
-        if (isNaN(value)) {
-            alert("invalid value")
-            return
-        }
-        await vyperStorage.methods.set(value).send({from: accounts[0]})
-            .on('receipt', async () => {
-                this.setState({
-                    vyperValue: await vyperStorage.methods.get().call()
-                })
-            })
-    }
+    // changeVyper = async (e) => {
+    //     const {accounts, vyperStorage, vyperInput} = this.state
+    //     e.preventDefault()
+    //     const value = parseInt(vyperInput)
+    //     if (isNaN(value)) {
+    //         alert("invalid value")
+    //         return
+    //     }
+    //     await vyperStorage.methods.set(value).send({from: accounts[0]})
+    //         .on('receipt', async () => {
+    //             this.setState({
+    //                 vyperValue: await vyperStorage.methods.get().call()
+    //             })
+    //         })
+    // }
 
-    changeSolidity = async (e) => {
-        const {accounts, solidityStorage, solidityInput} = this.state
-        e.preventDefault()
-        const value = parseInt(solidityInput)
-        if (isNaN(value)) {
-            alert("invalid value")
-            return
-        }
-        await solidityStorage.methods.set(value).send({from: accounts[0]})
-            .on('receipt', async () => {
-                this.setState({
-                    solidityValue: await solidityStorage.methods.get().call()
-                })
-            })
-    }
+    // changeSolidity = async (e) => {
+    //     const {accounts, solidityStorage, solidityInput} = this.state
+    //     e.preventDefault()
+    //     const value = parseInt(solidityInput)
+    //     if (isNaN(value)) {
+    //         alert("invalid value")
+    //         return
+    //     }
+    //     await solidityStorage.methods.set(value).send({from: accounts[0]})
+    //         .on('receipt', async () => {
+    //             this.setState({
+    //                 solidityValue: await solidityStorage.methods.get().call()
+    //             })
+    //         })
+    // }
 
     render() {
-        const {
-            web3, accounts, chainid,
-            vyperStorage, vyperValue, vyperInput,
-            solidityStorage, solidityValue, solidityInput
-        } = this.state
-
-        if (!web3) {
+        if (!this.state.loaded) {
             return <div>Loading Web3, accounts, and contracts...</div>
         }
 
         // <=42 to exclude Kovan, <42 to include Kovan
-        if (isNaN(chainid) || chainid < 42) {
+        if (isNaN(this.chainid) || this.chainid < 42) {
             return <div>Wrong Network! Switch to your local RPC "Localhost: 8545" in your Web3 provider (e.g. Metamask)</div>
         }
 
-        if (!vyperStorage || !solidityStorage) {
+        if (!this.MyToken || !this.MyTokenSale || !this.KycContract) {
             return <div>Could not find a deployed contract. Check console for details.</div>
         }
 
-        const isAccountsUnlocked = accounts ? accounts.length > 0 : false
+        const isAccountsUnlocked = this.accounts ? this.accounts.length > 0 : false
 
         return (<div className="App">
             <h1>Your Brownie Mix is installed and ready.</h1>
@@ -173,43 +152,17 @@ class App extends Component {
                     </p>
                     : null
             }
-            <h2>Vyper Storage Contract</h2>
-
-            <div>The stored value is: {vyperValue}</div>
+            <h2>My Token Contract</h2>
+            <div>The stored value is: {this.MyTokenValue}</div>
             <br/>
-            <form onSubmit={(e) => this.changeVyper(e)}>
-                <div>
-                    <label>Change the value to: </label>
-                    <br/>
-                    <input
-                        name="vyperInput"
-                        type="text"
-                        value={vyperInput}
-                        onChange={(e) => this.setState({vyperInput: e.target.value})}
-                    />
-                    <br/>
-                    <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
-                </div>
-            </form>
 
-            <h2>Solidity Storage Contract</h2>
-            <div>The stored value is: {solidityValue}</div>
+            <h2>My TokenSale Contract</h2>
+            <div>The stored value is: {this.MyTokenSaleValue}</div>
             <br/>
-            <form onSubmit={(e) => this.changeSolidity(e)}>
-                <div>
-                    <label>Change the value to: </label>
-                    <br/>
-                    <input
-                        name="solidityInput"
-                        type="text"
-                        value={solidityInput}
-                        onChange={(e) => this.setState({solidityInput: e.target.value})}
-                    />
-                    <br/>
-                    <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
 
-                </div>
-            </form>
+            <h2>My KYC Contract</h2>
+            <div>The stored value is: {this.KycContractValue}</div>
+            <br/>
         </div>)
     }
 }
