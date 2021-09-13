91363f14b72b03eccccf90468151601503f7ef57
sjoerdsommen <sjoerdsommen@gmail.com>
2021-09-13 17:17:26 +0200

    adjusted react app.js

--- react/client/src/App.js
+++ react/client/src/App.js
@@ -1,170 +1,123 @@
-import React, {Component} from "react"
-import './App.css'
+import React, { Component } from "react";
 import {getWeb3} from "./getWeb3"
 import map from "./artifacts/deployments/map.json"
-import {getEthereum} from "./getEthereum"
+
+import "./App.css";
 
 class App extends Component {
+  state = { loaded:false, kycAddress: "0x123...", tokenSaleAddress: null, userTokens:0 };
 
-    state = { loaded:false }
+  componentDidMount = async () => {
+    try {
+      // Get network provider and web3 instance.
+      this.web3 = await getWeb3();
 
-    componentDidMount = async () => {
+      // Use web3 to get the user's accounts.
+      this.accounts = await this.web3.eth.getAccounts();
 
-        // Get network provider and web3 instance.
-        this.web3 = await getWeb3()
+      // Get the contract instance.
+      this.networkId = await this.web3.eth.net.getId();
+      const chainid = parseInt(await this.web3.eth.getChainId());
 
-        // Try and enable accounts (connect metamask)
-        try {
-            this.ethereum = await getEthereum()
-            this.ethereum.enable()
-        } catch (e) {
-            console.log(`Could not enable accounts. Interaction with contracts not available.
-            Use a modern browser with a Web3 plugin to fix this issue.`)
-            console.log(e)
-        }
-
-        // Use web3 to get the user's accounts
-        this.accounts = await this.web3.eth.getAccounts()
-
-        // Get the current chain id
-        this.chainid = parseInt(await this.web3.eth.getChainId())
-
-        this.setState({}, await this.loadInitialContracts)
-
-    }
-
-    loadInitialContracts = async () => {
         // <=42 to exclude Kovan, <42 to include kovan
-        if (this.state.chainid < 42) {
-            // Wrong Network!
-            return
-        }
-        console.log(this.state.chainid)
-        
-        var _chainID = 0;
-        if (this.state.chainid === 42){
-            _chainID = 42;
-        }
-        if (this.state.chainid === 1337){
-            _chainID = "dev"
-        }
-        console.log(_chainID)
-        this.MyToken = await this.loadContract(_chainID,"MyToken")
-        this.MyTokenSale = await this.loadContract(_chainID,"MyTokenSale")
-        this.KycContract = await this.loadContract(_chainID,"KycContract")
+      if (chainid < 42) {
+          // Wrong Network!
+          return
+      }
+      console.log(chainid)
+      
+      var _chainID = 0;
+      if (chainid === 42){
+          _chainID = 42;
+      }
+      if (chainid === 1337){
+          _chainID = "dev"
+      }
 
-        if (!this.MyToken || !this.MyTokenSale || !this.KycContract) {
-            return
-        }
-
-        this.MyTokenValue = await this.MyToken.methods.get().call()
-        this.MyTokenSaleValue = await this.MyTokenSale.methods.get().call()
-        this.KycContractValue = await this.KycContract.methods.get().call()
-
-        this.setState({
-            loaded:true
-        })
+      this.tokenInstance = await this.loadContract(_chainID,"MyToken")
+      this.tokenSaleInstance = await this.loadContract(_chainID,"MyTokenSale")
+      this.kycInstance = await this.loadContract(_chainID,"KycContract")
+      
+      this.setState({loaded:true, tokenSaleAddress:map[_chainID]["MyTokenSale"][0]}, this.updateUserTokens);
+    } catch (error) {
+      // Catch any errors for any of the above operations.
+      alert(
+        `Failed to load web3, accounts, or contract. Check console for details.`,
+      );
+      console.error(error);
     }
+  };
 
-    loadContract = async (chain, contractName) => {
-        // Load a deployed contract instance into a web3 contract object
-        const {web3} = this.state
+  loadContract = async (chain, contractName) => {
+      // Load a deployed contract instance into a web3 contract object
+      const {web3} = this.state
 
-        // Get the address of the most recent deployment from the deployment map
-        let address
-        try {
-            address = map[chain][contractName][0]
-        } catch (e) {
-            console.log(`Couldn't find any deployed contract "${contractName}" on the chain "${chain}".`)
-            return undefined
-        }
+      // Get the address of the most recent deployment from the deployment map
+      let address
+      try {
+          address = map[chain][contractName][0]
+      } catch (e) {
+          console.log(`Couldn't find any deployed contract "${contractName}" on the chain "${chain}".`)
+          return undefined
+      }
 
-        // Load the artifact with the specified address
-        let contractArtifact
-        try {
-            contractArtifact = await import(`./artifacts/deployments/${chain}/${address}.json`)
-        } catch (e) {
-            console.log(`Failed to load contract artifact "./artifacts/deployments/${chain}/${address}.json"`)
-            return undefined
-        }
+      // Load the artifact with the specified address
+      let contractArtifact
+      try {
+          contractArtifact = await import(`./artifacts/deployments/${chain}/${address}.json`)
+      } catch (e) {
+          console.log(`Failed to load contract artifact "./artifacts/deployments/${chain}/${address}.json"`)
+          return undefined
+      }
 
-        return new web3.eth.Contract(contractArtifact.abi, address)
+      return new web3.eth.Contract(contractArtifact.abi, address)
+  }
+
+  updateUserTokens = async () => {
+    let userTokens = await this.tokenInstance.methods.balanceOf(this.accounts[0]).call();
+    this.setState({userTokens: userTokens});
+  }
+
+  listenToTokenTransfer = () => {
+    this.tokenInstance.events.Transfer({to: this.accounts[0]}).on("data",this.updateUserTokens);
+  }
+
+  handleBuyTokens = async() => {
+    await this.tokenSaleInstance.methods.buyTokens(this.accounts[0]).send({from: this.accounts[0], value: this.web3.utils.toWei("1","wei")});
+  }
+
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleKycWhitelisting = async () => {
+    await this.kycInstance.methods.setKycCompleted(this.state.kycAddress).send({from: this.accounts[0]});
+    alert("KYC for "+this.state.kycAddress+" is completed");
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return <div>Loading Web3, accounts, and contract...</div>;
     }
-
-    // changeVyper = async (e) => {
-    //     const {accounts, vyperStorage, vyperInput} = this.state
-    //     e.preventDefault()
-    //     const value = parseInt(vyperInput)
-    //     if (isNaN(value)) {
-    //         alert("invalid value")
-    //         return
-    //     }
-    //     await vyperStorage.methods.set(value).send({from: accounts[0]})
-    //         .on('receipt', async () => {
-    //             this.setState({
-    //                 vyperValue: await vyperStorage.methods.get().call()
-    //             })
-    //         })
-    // }
-
-    // changeSolidity = async (e) => {
-    //     const {accounts, solidityStorage, solidityInput} = this.state
-    //     e.preventDefault()
-    //     const value = parseInt(solidityInput)
-    //     if (isNaN(value)) {
-    //         alert("invalid value")
-    //         return
-    //     }
-    //     await solidityStorage.methods.set(value).send({from: accounts[0]})
-    //         .on('receipt', async () => {
-    //             this.setState({
-    //                 solidityValue: await solidityStorage.methods.get().call()
-    //             })
-    //         })
-    // }
-
-    render() {
-        if (!this.state.loaded) {
-            return <div>Loading Web3, accounts, and contracts...</div>
-        }
-
-        // <=42 to exclude Kovan, <42 to include Kovan
-        if (isNaN(this.chainid) || this.chainid < 42) {
-            return <div>Wrong Network! Switch to your local RPC "Localhost: 8545" in your Web3 provider (e.g. Metamask)</div>
-        }
-
-        if (!this.MyToken || !this.MyTokenSale || !this.KycContract) {
-            return <div>Could not find a deployed contract. Check console for details.</div>
-        }
-
-        const isAccountsUnlocked = this.accounts ? this.accounts.length > 0 : false
-
-        return (<div className="App">
-            <h1>Your Brownie Mix is installed and ready.</h1>
-            <p>
-                If your contracts compiled and deployed successfully, you can see the current
-                storage values below.
-            </p>
-            {
-                !isAccountsUnlocked ?
-                    <p><strong>Connect with Metamask and refresh the page to
-                        be able to edit the storage fields.</strong>
-                    </p>
-                    : null
-            }
-            <h2>My Token Contract</h2>
-            <div>The stored value is: {this.MyTokenValue}</div>
-            <br/>
-
-            <h2>My TokenSale Contract</h2>
-            <div>The stored value is: {this.MyTokenSaleValue}</div>
-            <br/>
-
-            <h2>My KYC Contract</h2>
-            <div>The stored value is: {this.KycContractValue}</div>
-            <br/>
-        </div>)
-    }
+    return (
+      <div className="App">
+        <h1>StarDucks Cappucino Token Sale</h1>
+        <p>Get your Tokens today!</p>
+        <h2>Kyc Whitelisting</h2>
+        Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
+        <button type="button" onClick={this.handleKycWhitelisting}>Add to Whitelist</button>
+        <h2>Buy Tokens</h2>
+        <p>If you want to buy tokens, send Wei to this address: {this.state.tokenSaleAddress}</p>
+        <p>You currently have: {this.state.userTokens} CAPPU Tokens</p>
+        <button type="button" onClick={this.handleBuyTokens}>Buy more tokens</button>
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
